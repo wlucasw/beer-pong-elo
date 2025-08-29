d756c86000651bc6db6d2c6e07a0dc5cd7a8f7f6
wlucaswPC <lucas.panier@ivcevidensia.com>
2025-08-30 00:20:16 +0200

    feature: change player input to select

--- src/routes/match/setup/+page.svelte
+++ src/routes/match/setup/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-	import { Button, Input, Card } from 'flowbite-svelte';
+	import { Button, Input, Card, MultiSelect } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import type { Player } from '../../../domain/Player';
 
-	let teamAmine = '';
-	let teamRobin = '';
+	let teamAmine: string[] = [];
+	let teamRobin: string[] = [];
 	let ballNumber: number | '' = '';
+	let players: Player[] = [];
+	let playerOptions: { value: string; name: string }[] = [];
+
+	onMount(async () => {
+		const res = await fetch('/api/player');
+		players = await res.json();
+		playerOptions = players.map((player) => ({
+			value: player.id.toString(), // or another unique identifier
+			name: player.name // add the required 'name' property
+		}));
+	});
 
 	function handleSubmit() {
 		// Handle form submission logic here
@@ -13,19 +26,27 @@
 <main class="flex justify-center p-6">
 	<Card class="w-full max-w-none space-y-4 p-6">
 		<h2 class="text-center text-xl font-bold">🎯 Match Setup</h2>
-		<form class="space-y-4" on:submit|preventDefault={handleSubmit}>
-			<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-				<Input placeholder="Team Amine side" bind:value={teamAmine} />
-				<Input placeholder="Team Robin side" bind:value={teamRobin} />
-			</div>
-			<Input
-				type="number"
-				min="1"
-				placeholder="Nombre de balles"
-				bind:value={ballNumber}
-				class="w-full"
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+			<MultiSelect
+				items={playerOptions}
+				value={teamAmine}
+				size="lg"
+				placeholder="Team Amine side"
 			/>
-			<Button type="submit" class="w-full">Valider</Button>
-		</form>
+			<MultiSelect
+				items={playerOptions}
+				value={teamRobin}
+				size="lg"
+				placeholder="Team Robin side"
+			/>
+		</div>
+		<Input
+			type="number"
+			min="1"
+			placeholder="Nombre de balles"
+			bind:value={ballNumber}
+			class="w-full"
+		/>
+		<Button type="submit" class="w-full">Valider</Button>
 	</Card>
 </main>
